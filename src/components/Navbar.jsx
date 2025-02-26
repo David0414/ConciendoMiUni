d507b774415e36a5bd57a04e8ee713385b7af915
David0414 <david.mendoza.perez74@gmail.com>
2025-02-25 20:16:35 -0600

    Solucion menu hamburguesa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
 import { Link } from 'react-router-dom';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark px-3">
       <Link className="navbar-brand" to="/">Inicio</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <button 
+        className="navbar-toggler" 
+        type="button" 
+        data-bs-toggle="collapse" 
+        data-bs-target="#navbarNav" 
+        aria-controls="navbarNav" 
+        aria-expanded="false" 
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
@@ -15,27 +25,23 @@ function Navbar() {
           <li className="nav-item">
             <Link className="nav-link" to="/sombrero">SOMBRERO</Link>
           </li>
-
           <li className="nav-item">
             <Link className="nav-link" to="/catwda">CATWDA</Link>
           </li>
-
           <li className="nav-item">
             <Link className="nav-link" to="/ishikawa">ISHIKAWA</Link>
           </li>
-
           <li className="nav-item">
             <Link className="nav-link" to="/resumen">RESÚMEN</Link>
           </li>
-
           <li className="nav-item">
             <Link className="nav-link" to="/actividad">ACTIVIDAD</Link>
           </li>
-
         </ul>
       </div>
     </nav>
   );
 }
+
 
 export default Navbar;
